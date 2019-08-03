96b9a6a4d810e07957022a4fa73691e1f8e7475f
unknown <samuel_newhouse@yahoo.com>
2019-08-03 14:57:33 -0500

    Added SphereCastAll for ground detection.

--- Assets/Scripts/PhysPlayer.cs
+++ Assets/Scripts/PhysPlayer.cs
@@ -5,15 +5,16 @@ public class PhysPlayer : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5;
     [SerializeField] float mouseSensitivity = 2;
+    [SerializeField] float slopeLimit = 44;
 
     Transform       camTransform;
     CapsuleCollider capsule;
-    Rigidbody       body;    
+    Rigidbody       body;
 
     Vector3 sphereCastOffset;
-    float sphereCastDistance = 0.1f;
-    float mouseX = 0f;
-    float mouseY = 0f;
+    float sphereCastDistance = 0.0001f;
+    float mouseX;
+    float mouseY;
     
     Rigidbody pushingRigidbody = null;
 
@@ -28,23 +29,24 @@ public class PhysPlayer : MonoBehaviour
         mouseX = transform.rotation.eulerAngles.y;
         mouseY = camTransform.rotation.eulerAngles.x;
 
-        // Bottom edge of cast sphere will start slightly above capsule bottom.
-        float sphereCastOffsetY = -capsule.height / 2 + capsule.radius + .05f;
+        // -- Bottom edge of cast sphere will start slightly above capsule bottom.
+        float sphereCastOffsetY = -capsule.height / 2 + capsule.radius + .00005f;
         sphereCastOffset = new Vector3(0, sphereCastOffsetY, 0);
     }
 
     void Update()
     {
-        Rotation(); // Camera movement should be as responsive as possible.
+        Rotation(); // -- Camera movement should be as responsive as possible.
     }
 
     void FixedUpdate()
     {        
-        Movement(); // Body movement should sync perfectly with physics.
+        Movement(); // -- Body movement should sync perfectly with physics.
     }
 
     void Rotation()
     {
+        // @TODO: Invert Axis Option
         mouseX += Input.GetAxis("Mouse X") * mouseSensitivity;
         mouseY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
         mouseY = Mathf.Clamp(mouseY, -89.9f, 89.9f);
@@ -66,8 +68,26 @@ public class PhysPlayer : MonoBehaviour
 
         // -- Check for ground.
         RaycastHit hit;
+        Vector3 lowestGroundNormal = Vector3.zero; // Lowest hit point that's under slopeLimit.
         Vector3 bottom = transform.position + sphereCastOffset;
-        bool onGround = Physics.SphereCast(bottom, capsule.radius, Vector3.down, out hit, sphereCastDistance);
+        bool onGround = false; //Physics.SphereCast(bottom, capsule.radius, Vector3.down, out hit, sphereCastDistance);
+
+        // -- Need to SphereCastAll because the player might be "on" a very steep slope when they should really be considered on a
+        //    lesser slope that's further below them. Anything over the slopeLimit will be ignored for determining onGround.
+        RaycastHit[] hits = Physics.SphereCastAll(bottom, capsule.radius, Vector3.down, sphereCastDistance);
+
+        float lowestY = Mathf.Infinity;
+
+        foreach (RaycastHit h in hits)
+        {            
+            if (h.rigidbody != body && Vector3.Angle(Vector3.up, h.normal) <= slopeLimit && h.point.y < lowestY)
+            {
+                lowestY = h.point.y;
+                lowestGroundNormal = h.normal;
+                onGround = true;
+            }
+        }
+
 
         if (onGround)
         {
@@ -79,12 +99,16 @@ public class PhysPlayer : MonoBehaviour
             }
 
             // -- Create vector parallel to ground slope
-            if (hit.normal != Vector3.up)
-                direction = Vector3.Normalize(direction + Vector3.Reflect(direction, hit.normal));
+            if (lowestGroundNormal != Vector3.up)
+                direction = Vector3.Normalize(direction + Vector3.Reflect(direction, lowestGroundNormal));
 
             // -- Apply friction and force
-            Vector3 friction = body.velocity * (noInput ? -2400 : -500);
+            Vector3 friction = body.velocity * (noInput ? -3400 : -500);
             body.AddForce(friction + direction * 2400);
+        }
+        else if(body.IsSleeping()) // Objects can slide out from under player after player sleeps.
+        {
+            body.WakeUp();
         }
 
         /*
@@ -98,13 +122,14 @@ public class PhysPlayer : MonoBehaviour
         */
     }
 
+    /*
     void OnCollisionEnter(Collision collision)
     {
         Rigidbody otherBody = collision.rigidbody;
         if (Input.GetAxis("Vertical") <= 0 || otherBody == null || otherBody.isKinematic)
             return;
 
-        // Only push object if it collides within a certain vertical distance of player center.
+        // -- Only push object if it collides within a certain vertical distance of player center.
         Vector3 ownPosition = transform.position;        
         Vector3 closestPoint = collision.collider.ClosestPoint(ownPosition);        
         if (Mathf.Abs(closestPoint.y - ownPosition.y) < 0.3)
@@ -116,4 +141,5 @@ public class PhysPlayer : MonoBehaviour
         if (collision.rigidbody == pushingRigidbody)
             pushingRigidbody = null;
     }
+    */
 }
