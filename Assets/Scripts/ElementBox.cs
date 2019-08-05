c7c44ebb4afa116a9f351078c541cd43a31e6f65
unknown <samuel_newhouse@yahoo.com>
2019-08-05 18:26:19 -0500

    Get basic stone element manifest working

--- Assets/Scripts/ElementBox.cs
+++ Assets/Scripts/ElementBox.cs
@@ -2,117 +2,146 @@
 
 public class ElementBox : MonoBehaviour
 {
-    enum Element
+    public enum Element
     {
         Void, Balance, Order, Chaos, Stone, Lightning, Ice, Fire, Water, Wind, Shadow, Light, Death, Life
     }
 
-    enum Category
+    public enum Category
     {
         Void, Balance, Order, Chaos
     }
 
-    enum Location
+    public enum Location
     {
         Void, Point, Material, Force
     }
 
-    enum LocationSubtype
+    public enum LocationSubtype
     {
         None, Base, Transcendent
     }
 
-    [SerializeField] Element element;
-    [SerializeField] Category category;
-    [SerializeField] Location location;
-    [SerializeField] LocationSubtype locationSubtype;    
+    [SerializeField] public Element element;
+    [SerializeField] public Category category;
+    [SerializeField] public Location location;
+    [SerializeField] public LocationSubtype locationSubtype;
+    [SerializeField] public GameObject manifestObject;
 
-    delegate void ElementBoxCollisionHandler(ElementBox thisBox, ElementBox otherBox);
-    ElementBoxCollisionHandler handler;
+    private delegate void ElementBoxCollisionHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject = null);
+    private ElementBoxCollisionHandler handler;
 
-    static void VoidHandler(ElementBox thisBox, ElementBox otherBox)
+    private static float groundScanDistance = 1f;
+    private static int ElementBoxesLayer = 1 << 8;
+
+    private static void spawnElementManifest(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
+    {
+        // -- Start at average position between boxes.
+        Vector3 startPosition = (thisBox.transform.position + otherBox.transform.position) / 2;
+        Vector3 thisVelocity = thisBox.GetComponentInParent<Rigidbody>().velocity;
+        Vector3 otherVelocity = otherBox.GetComponentInParent<Rigidbody>().velocity;
+
+        // -- Use highest magnitude velocity to decide on direction.
+        Vector3 manifestDirection = (thisVelocity.magnitude >= otherVelocity.magnitude) ? thisVelocity : otherVelocity;
+        manifestDirection = Vector3.Normalize(manifestDirection);
+
+        // -- Align along any nearby ground surfaces
+        RaycastHit hit;
+        if (Physics.Raycast(startPosition, Vector3.down, out hit, groundScanDistance, ~ElementBoxesLayer))
+            manifestDirection = Vector3.Normalize(manifestDirection + Vector3.Reflect(manifestDirection, hit.normal));
+
+        Quaternion manifestRotation = Quaternion.LookRotation(manifestDirection, hit.normal);
+        Instantiate(manifestObject, startPosition, manifestRotation);
+
+        Object.Destroy(otherBox.gameObject);
+        Object.Destroy(thisBox.gameObject);
+    }
+
+    private static void VoidHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.category != Category.Void && otherBox.category != Category.Balance)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void BalanceHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void BalanceHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         // -- Balance intentionally does nothing.
     }
 
-    static void OrderHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void OrderHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Chaos)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void ChaosHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void ChaosHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Order)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void StoneHandler(ElementBox thisBox, ElementBox otherBox) {
+    private static void StoneHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject) {
         if (otherBox.element == Element.Lightning)
             Object.Destroy(otherBox.gameObject);
+        else if(otherBox.element == Element.Order)
+            spawnElementManifest(thisBox, otherBox, manifestObject);
     }
 
-    static void LightningHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void LightningHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Stone)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void IceHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void IceHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Fire)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void FireHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void FireHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Ice)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void WaterHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void WaterHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Wind)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void WindHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void WindHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Water)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void ShadowHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void ShadowHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Light)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void LightHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void LightHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Shadow)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void DeathHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void DeathHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Life)
             Object.Destroy(otherBox.gameObject);
     }
 
-    static void LifeHandler(ElementBox thisBox, ElementBox otherBox)
+    private static void LifeHandler(ElementBox thisBox, ElementBox otherBox, GameObject manifestObject)
     {
         if (otherBox.element == Element.Death)
             Object.Destroy(otherBox.gameObject);
     }
 
-    void Start()
+    private void Awake()
     {
         switch (element)
         {
@@ -159,15 +188,15 @@ public class ElementBox : MonoBehaviour
                 handler = LifeHandler;
                 break;
             default: // @TODO Default to avoid NullReferenceException while creating. Might change/remove later?
-                handler = BalanceHandler; 
+                handler = BalanceHandler;
                 break;
         }
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         ElementBox elementBox = collision.gameObject.GetComponent<ElementBox>();
         if (elementBox)
-            handler(this, elementBox);
+            handler(this, elementBox, manifestObject);
     }
 }
